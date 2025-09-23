a151fd9557a4dd268ca445eb8b43fec55e9cc98f
Gugan <131288000+Gugan-23@users.noreply.github.com>
2025-09-23 18:13:01 +0530

    Update LogoSlider.js.

--- frontend/src/components/LogoSlider/LogoSlider.js
+++ frontend/src/components/LogoSlider/LogoSlider.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./LogoSlider.css";
 
 const LogoSlider = () => {
+  // Paths must match exactly (case-sensitive and spaces included!)
   const logos = [
-    "anv-ftr-1.png", "anv-ftr-2.png", "anv-ftr-3.png", "anv-ftr-4.png",
-    "anv-ftr-5.png", "anv-ftr-6.png", "anv-ftr-7.png", "anv-ftr-8.png",
+    "/Certificate images/Certificate images/fssai.png",
+    "/Certificate images/Certificate images/msme.png",
   ];
 
   return (
@@ -14,8 +15,8 @@ const LogoSlider = () => {
           {logos.map((file, idx) => (
             <div className="logo-slide" key={idx}>
               <img
-                src={`https://www.anveshan.farm/cdn/shop/files/${file}?v=1729241487&width=480`}
-                alt=""
+                src={file}
+                alt={`logo-${idx}`}
                 loading="lazy"
               />
             </div>
