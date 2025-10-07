e580f71ad19505d623549731c7acc18794a9b2c5
Gugan <131288000+Gugan-23@users.noreply.github.com>
2025-10-07 11:50:36 +0530

    Update image.model.js

--- backend/src/models/image.model.js
+++ backend/src/models/image.model.js
@@ -1,9 +1,13 @@
+// models/image.model.js
 import mongoose from "mongoose";
 
 const imageSchema = new mongoose.Schema({
   imageUrl: { type: String, required: true },
-  publicId: { type: String, required: true }
+  publicId: { type: String, required: true },
 });
 
-const Image = mongoose.model("Image", imageSchema);
-export default Image;
+// For desktop
+export const Image = mongoose.model("Image", imageSchema);
+
+// For mobile
+export const Image2 = mongoose.model("Image2", imageSchema);
